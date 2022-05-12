73f7d7119e94ce471caffebd2285db126a21a5bc
georgia-martinez <gcm49@case.edu>
2022-05-12 00:48:56 -0500

    Course prereqs displayed in graph

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import networkx as nx
+import matplotlib.pyplot as plt
+
 from subject import *
 from course import *
+from node import *
 
 all_subjects = dict()
 
@@ -23,9 +27,14 @@ def create_subject_map():
 
         all_subjects[code] = Subject(code, name, "https://bulletin.case.edu" + tag["href"])
 
-def course_reqs(course_input):
+    all_subjects["EECS"] = all_subjects["CSDS"] # Old course code that sometimes pops up
 
-    input_code = re.search("[A-Z]{4}\s[0-9]{3}", course_input).group(0)
+all_courses = dict()
+
+def find_course_reqs(course_input):
+
+    code_regex = "[A-Z]{4}\s[0-9]{3}[A-Z]?"
+    input_code = re.search(code_regex, course_input).group(0)
     subject = re.search("[A-Z]{4}", input_code).group(0)
 
     # Get all courses for a specific subject
@@ -40,14 +49,14 @@ def course_reqs(course_input):
     for tag in tags:
         course_header = tag.find("p", class_="courseblocktitle").find("strong").string
 
-        code = re.search("[A-Z]{4}\s[0-9]{3}", course_header).group(0)
+        code = re.search(code_regex, course_header).group(0)
         code = code.replace("\xa0", " ")  # remove hard spaces
 
         if code == input_code:
             break
 
-    if code is None:
-        print("ERROR: Code not found")
+    if code != input_code:
+        print(f"ERROR: {input_code} not found")
         return
 
     # Get course name, credit hours, and description
@@ -63,29 +72,79 @@ def course_reqs(course_input):
     course = Course(name, code, credit_hours, description)
 
     # Find the prerequisites
-    prereqs_text = re.search("Prereq:[\sa-zA-Z0-9]*", description)
+    req_regex = "[\sa-zA-Z0-9\(\)/]*"
+
+    prereqs_text = re.search("Prereq:" + req_regex, description)
     prereqs = prereqs_text.group(0) if prereqs_text is not None else ""
 
     prereqs = prereqs.split(" and ")
 
     for req in prereqs:
-        courses = re.findall("[A-Z]{4}\s[0-9]{3}", req)
+        courses = re.findall(code_regex, req)
 
         if courses:
             course.prereqs.append(courses)
 
     # Find the corequisites
-    coreqs_text = re.search("Coreq:[\sa-zA-Z0-9]*", description)
+    coreqs_text = re.search("Coreq:" + req_regex, description)
     coreqs = coreqs_text.group(0) if coreqs_text is not None else "None"
 
-    courses = re.findall("[A-Z]{4}\s[0-9]{3}", coreqs)
+    courses = re.findall(code_regex, coreqs)
 
     if courses:
         course.coreqs.append(courses)
 
-    return course
+    # Update course dictionary
+    if course.code not in all_courses:
+        all_courses[course.code] = course
+
+        # Call function on all prereqs and coreqs
+        for group in course.prereqs:
+            for prereq in group:
+                find_course_reqs(prereq)
+
+        for group in course.coreqs:
+            for coreq in group:
+                find_course_reqs(coreq)
+
+def create_graph(root):
+    graph = nx.DiGraph()
+
+    nodes = dict()
+
+    for key in all_courses:
+        nodes[key] = Node(all_courses[key])
+        graph.add_node(all_courses[key].code)
+
+    stack = [nodes[root.code]]
+
+    while stack:
+        curr_node = stack[-1]
+        stack.pop()
+
+        if not curr_node.visited:
+            curr_node.visited = True
+
+        for group in curr_node.course.prereqs:
+            for prereq in group:
+                graph.add_edge(all_courses[prereq].code, curr_node.course.code)
+
+                if not nodes[prereq].visited:
+                    stack.append(nodes[prereq])
+
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos, node_size=500)
+    nx.draw_networkx_edges(graph, pos, edgelist=graph.edges(), edge_color="black")
+    nx.draw_networkx_labels(graph, pos)
+
+    plt.show()
 
 if __name__ == "__main__":
     create_subject_map()
-    course = course_reqs("MATH 319")
-    print(course.info())
+
+    course_code = "MATH 380"
+
+    find_course_reqs(course_code)
+    course = all_courses[course_code]
+
+    create_graph(course)
